19d48ef2c98741d8eedbf5a0f5bc603b3a595fcd
LowEyeQ <norrapat10@gmail.com>
2022-11-09 21:19:17 +0700

    first commit

---
+++ PSCP semter 1/AB.py
@@ -0,0 +1,7 @@
+"""[Pre] AB"""
+def main():
+    """[Pre] AB"""
+    num = int(input())
+    num1 = int(input())
+    print(num+num1)
+main()
---
+++ PSCP semter 1/Kayak.py
@@ -0,0 +1,12 @@
+"""[Pre] kayak"""
+def main():
+    """[Pre] kayak"""
+    num = int(input())
+    kayak1 = 2
+    kayak2 = num-1
+    lis = []
+    for o in range(num*2):
+     o *= num
+     lis.append(input())
+
+main()
