13fde81fcbcb4db03e470bdf8f7e507826e48640
LowEyeQ <norrapat10@gmail.com>
2022-12-09 23:47:16 +0700

    last

--- PSCP semter 1/237 NumDays.py
+++ PSCP semter 1/237 NumDays.py
@@ -1,13 +1,24 @@
-import calendar as calen
-"""NumDays"""
-def numday(day_1, month_1, day_2, month_2):
-    """NumDays"""
-    obj = calen.Calendar()
-    for day in obj.itermonthdays3(2020, month_1):
-        print(day)
+"""IG: few.pz"""
+def add(month):
+    """ Add days """
+    manyday = 0
+    for i in range(1, month):
+        if i == 2:
+            manyday += 28
+        elif i == 4 or i == 6 or i == 9 or i == 11:
+            manyday += 30
+        else:
+            manyday += 31
+    return manyday
 
-
-
-
-
-numday(int(input()), int(input()), int(input()), int(input()))
+def main():
+    """ Main function """
+    day1, mon1, day2, mon2 = int(input()), int(input()), int(input()), int(input())
+    year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if (day1 > year[mon1-1]) or (day2 > year[mon2-1]):
+        print("Impossible")
+    else:
+        days1 = add(mon1)+day1
+        days2 = add(mon2)+day2
+        print(abs(days1-days2))
+main()
--- PSCP semter 1/239 Heads and Legs.py
+++ PSCP semter 1/239 Heads and Legs.py
@@ -1,12 +1,12 @@
 """Heads and Legs"""
-def hal(rab_bird, legs):
-    """Heads and Legs"""
-    if rab_bird == 0 and legs == 0:
-        print(0, 0)
-    elif legs//4 > rab_bird:
+def hal():
+    """dddd"""
+    head = int(input())
+    leg = int(input())
+    rabbit, ghost = divmod(leg-2 * head, 2)
+    bird = head - rabbit
+    if rabbit >= 0 and bird >= 0 and ghost == 0:
+        print(rabbit, bird)
+    else:
         print("Impossible")
-
-
-
-
-hal(int(input()), int(input()))
+hal()
